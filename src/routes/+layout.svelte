64ad424629a9bd3fb0e921176d33623dce235b07
Dang Khoa Chiem <dk@chiem.me>
2024-09-14 18:29:25 -0400

    fix merge conflict

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,24 +16,15 @@
 		height: calc(100% - 100px);
 	}
 	.prompt {
-<<<<<<< HEAD
 		width: 28ch;
-		letter-spacing: 0.1em;
-=======
-		width:28ch;
 		//letter-spacing: .1em;
->>>>>>> 5c56bcb44d7e8fb12780534a520b896e15151942
 		border-right: 3px solid;
 		white-space: nowrap;
 		overflow: hidden;
 		font-size: 1em;
-<<<<<<< HEAD
 		animation:
 			typing 2s steps(28),
-			blink 0.5 step-end infinite alternate;
-=======
-		animation: typing 2s steps(28), blink .5s step-end infinite alternate;
->>>>>>> 5c56bcb44d7e8fb12780534a520b896e15151942
+			blink 0.5s step-end infinite alternate;
 	}
 	@keyframes blink {
 		50% {
